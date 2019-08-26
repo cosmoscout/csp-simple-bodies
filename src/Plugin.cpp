a7137023a88e1454269639fdb880d1af4ddd07ed
Jonas Gilg <jonas.gilg@dlr.de>
2019-08-26 16:49:02 +0200

    :wrench: Added error checks to the core settings.

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -57,8 +57,10 @@ void Plugin::init() {
           "There is no Anchor \"" + bodySettings.first + "\" defined in the settings.");
     }
 
-    double tStartExistence = cs::utils::convert::toSpiceTime(anchor->second.mStartExistence);
-    double tEndExistence   = cs::utils::convert::toSpiceTime(anchor->second.mEndExistence);
+
+    auto existence = cs::utils::convert::getExistenceFromSettings(*anchor);
+    double tStartExistence = existence.first;
+    double tEndExistence = existence.second;
 
     auto body = std::make_shared<SimpleBody>(bodySettings.second.mTexture, anchor->second.mCenter,
         anchor->second.mFrame, tStartExistence, tEndExistence);
