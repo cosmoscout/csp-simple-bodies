a29b5dda64826d7fa61ab125eb6abf5dae8f01a0
Simon Schneegans <code@simonschneegans.de>
2020-01-30 07:21:16 +0100

    :wrench: Use set_default_logger in dll

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -47,7 +47,7 @@ void from_json(const nlohmann::json& j, Plugin::Settings& o) {
 
 Plugin::Plugin() {
   // Create default logger for this plugin.
-  cs::utils::logger::init("csp-simple-bodies");
+  spdlog::set_default_logger(cs::utils::logger::createLogger("csp-simple-bodies"));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
