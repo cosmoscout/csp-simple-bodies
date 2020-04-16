f95c547e86757134c55cf40826c41978904a3e8b
Simon Schneegans <code@simonschneegans.de>
2020-04-16 10:14:35 +0200

    :wrench: Don't use spdlog's registry

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -47,13 +47,6 @@ void from_json(const nlohmann::json& j, Plugin::Settings& o) {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-Plugin::Plugin() {
-  // Create default logger for this plugin.
-  spdlog::set_default_logger(cs::utils::logger::createLogger("csp-simple-bodies"));
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 void Plugin::init() {
 
   spdlog::info("Loading plugin...");
